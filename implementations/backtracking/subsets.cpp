e4aeba60b10beed8e864653d9631987e2ef2ea10
Hai Ou <ouha019@gmail.com>
2025-10-14 11:10:56 +1100

    subset backtracking implementation

--- implementations/backtracking/subsets.cpp
+++ implementations/backtracking/subsets.cpp
@@ -14,7 +14,8 @@ public:
       for (int i = 0; i < n; ++i) {
         std::vector<int> tmp = res[i];
         tmp.push_back(num);
-        res.push_back(tmp);
+        res.push_back(
+            std::move(tmp)); // rvalue reference for tmp; can be deleted / used
       }
     }
 
@@ -24,3 +25,35 @@ public:
 
 /* Remember: don't iterate over a variable that is being modified within the
  * iteration */
+
+class Solution_backtracking {
+public:
+  /* Key idea is to explore part of a decision tree, "backtrack" to previous
+   * breakpoint, and explore a new part of the decision tree
+   * This is like a dfs, which saves all leaf nodes
+   * */
+  std::vector<std::vector<int>> subsets(std::vector<int> &nums) {
+
+    std::vector<std::vector<int>> res = {};
+    std::vector<int> subset = {};
+
+    dfs(0, nums, subset, res);
+
+    return res;
+  }
+
+private:
+  void dfs(int i, const std::vector<int> &nums, std::vector<int> &subset,
+           std::vector<std::vector<int>> &res) {
+    if (i >= nums.size()) {
+      res.push_back(subset);
+      return;
+    }
+
+    subset.push_back(nums[i]);
+    dfs(i + 1, nums, subset, res);
+
+    subset.pop_back();
+    dfs(i + 1, nums, subset, res);
+  }
+};
