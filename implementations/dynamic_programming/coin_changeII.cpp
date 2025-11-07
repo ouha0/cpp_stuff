bd6ae5953bc1c9d82538057b411cce406b01bde8
Hai Ou <ouha019@gmail.com>
2025-11-07 20:10:44 +1100

    alternative coin change solutions

--- implementations/dynamic_programming/coin_changeII.cpp
+++ implementations/dynamic_programming/coin_changeII.cpp
@@ -1,7 +1,96 @@
 #include <stdio.h>
 #include <vector>
 
-class Solution {
+/* There are quite a few solutions to this problem; recursion with constraints,
+ * and dynamic programming with two states */
+
+class dfs_solution {
+public:
+  /* This is a the standard dfs solution, where we discover all possible paths,
+   * but with some constraints.
+   *
+   * dfs solution takes 3 parameters; current amount, coins, and integer
+   * representing which coins are allowed to be used
+   */
+
+  // The problem with this solution is that it is too slow
+  // and does lots of repeated work
+  int change(int amount, std::vector<int> &coins) {
+    std::sort(coins.begin(), coins.end());
+    int offset = 0;
+    return dfs(amount, coins, 0);
+  }
+
+private:
+  int dfs(int n, std::vector<int> &coins, int offset) {
+    if (n == 0) {
+      return 1;
+    } else if (n < 0 || offset >= coins.size() || coins[offset] > n) {
+      return 0;
+    }
+
+    int count = 0;
+    for (int i = offset; i < coins.size(); ++i) {
+      count += dfs(n - coins[i], coins, i);
+    }
+    return count;
+  }
+};
+
+class dfs_solution_2 {
+public:
+  int change(int amount, std::vector<int> &coins) {
+    std::sort(coins.begin(), coins.end());
+
+    return dfs(amount, 0, coins);
+  }
+
+private:
+  int dfs(int n, int offset, std::vector<int> &coins) {
+    if (n == 0) {
+      return 1;
+    }
+
+    if (offset >= coins.size()) {
+      return 0;
+    }
+
+    int res = 0;
+    if (n >= coins[offset]) {
+      res += dfs(n - coins[offset], offset, coins);
+      res += dfs(n, offset + 1, coins);
+    }
+
+    return res;
+  }
+};
+
+class general_dp_solution {
+public:
+  int change(int amount, std::vector<int> &coins) {
+    std::vector<std::vector<int>> dp(coins.size() + 1,
+                                     std::vector<int>(amount + 1, 0));
+
+    for (int i = 0; i < coins.size() + 1; ++i) {
+      dp[i][0] = 1;
+    }
+
+    for (int i = coins.size() - 1; i >= 0; --i) {
+      int curr_coin = coins[i];
+      // Not the easy off by one error here for amount...
+      for (int j = 1; j < amount + 1; ++j) {
+        dp[i][j] = dp[i + 1][j];
+        if (j - curr_coin >= 0) {
+          dp[i][j] += dp[i][j - curr_coin];
+        }
+      }
+    }
+
+    return dp[0][amount];
+  }
+};
+
+class optimized_dp_Solution {
   /* The key idea to this problem is realising that this is a decision tree, but
    * slightly different and contrained. So there are paths that you are not
    * allowed to take, otherwise it would result in i.e. if we want to make the
---
+++ implementations/dynamic_programming/target_sum.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <vector>
+
+/* Again, there are many different solutions to this problem; general
+ * recursive decision tree, recursive tree with caching, and pure
+ * dynamic programming approach*/
+class Solution {
+public:
+  int findTargetSumWays(std::vector<int> &nums, int target) {
+
+  }
+};
